acb9227f005250b637ad3e38e1fbdd667434f0bf
Roshan Kumar Shukla <btech10189.21@bitmesra.ac.in>
2024-07-21 00:06:58 +0530

    first commit

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const connect = require('./db');
 const app = require('./index');
 require("dotenv").config();
-const PORT = 5000 || process.env.PORT;
+const PORT = process.env.PORT || 5000;
 
 
 app.listen(PORT, async () => {
